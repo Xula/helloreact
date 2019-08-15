c1f0735c84e118a91366244be64c34bac7ed3092
André Pedruzzi <andrerpedruzzi@gmail.com>
2019-08-15 20:43:28 -0300

    Updates

---
+++ precache-manifest.1a1bfab9c978635977a3a3d70003f202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23997a543996628f5cf176168c026c39",
+    "url": "/helloreact/index.html"
+  },
+  {
+    "revision": "1c92d9ed2ba7e6de5c6b",
+    "url": "/helloreact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2773d243bb975ea951b3",
+    "url": "/helloreact/static/js/2.dba262ed.chunk.js"
+  },
+  {
+    "revision": "1c92d9ed2ba7e6de5c6b",
+    "url": "/helloreact/static/js/main.df3a3f33.chunk.js"
+  },
+  {
+    "revision": "526774998909696c25ba",
+    "url": "/helloreact/static/js/runtime~main.1f409a07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
